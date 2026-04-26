b761ec3a0b24745fe70b3bf5e296fafefd115836
Andrea Latorre <latorre.andrea.93@gmail.com>
2026-04-26 10:12:50 +0200

    feat: add initial migration file to merge previous migrations in achievements app

---
+++ achievements/migrations/0004_merge.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('achievements', '0002_remove_userachievement_source'),
+        ('achievements', '0003_remove_achievementdefinition_description'),
+    ]
+
+    operations = []
